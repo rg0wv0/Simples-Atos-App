a8ba21d45ce7250f1c2469d59b0ff57b36947057
rg0wv0 <60630406+rg0wv0@users.noreply.github.com>
2021-11-02 17:06:52 -0300

    New Updates

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -8,7 +8,28 @@ import { store, persistor } from '../redux/store';
 
 import Home from '../screens/Home';
 import Comousar from '../screens/Comousar';
+
 import Ato1 from '../screens/Ato1';
+import Ato2 from '../screens/Ato2';
+import Ato3 from '../screens/Ato3';
+import Ato4 from '../screens/Ato4';
+import Ato5 from '../screens/Ato5';
+import Ato6 from '../screens/Ato6';
+import Ato7 from '../screens/Ato7';
+import Ato8 from '../screens/Ato8';
+import Ato9 from '../screens/Ato9';
+import Ato10 from '../screens/Ato10';
+import Ato11 from '../screens/Ato11';
+import Ato12 from '../screens/Ato12';
+import Ato13 from '../screens/Ato13';
+import Ato14 from '../screens/Ato14';
+import Ato15 from '../screens/Ato15';
+import Ato16 from '../screens/Ato16';
+import Ato17 from '../screens/Ato17';
+import Ato18 from '../screens/Ato18';
+import Ato19 from '../screens/Ato19';
+import Ato20 from '../screens/Ato20';
+
 import Listaatos from '../screens/Listaatos';
 
 const MainStack = createStackNavigator();
@@ -30,6 +51,120 @@ const MainStackScreen = () => (
     <MainStack.Screen 
       name="Ato1" 
       component={Ato1}
+      options={{ headerShown: false }}  
+    />
+
+    <MainStack.Screen 
+      name="Ato2" 
+      component={Ato2}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato3" 
+      component={Ato3}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato4" 
+      component={Ato4}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato5" 
+      component={Ato5}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato6" 
+      component={Ato6}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato7" 
+      component={Ato7}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato8" 
+      component={Ato8}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato9" 
+      component={Ato9}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato10" 
+      component={Ato10}
+      options={{ headerShown: false }}  
+    />
+        
+    <MainStack.Screen 
+      name="Ato11" 
+      component={Ato11}
+      options={{ headerShown: false }}  
+    />
+        
+    <MainStack.Screen 
+      name="Ato12" 
+      component={Ato12}
+      options={{ headerShown: false }}  
+    />
+            
+    <MainStack.Screen 
+      name="Ato13" 
+      component={Ato13}
+      options={{ headerShown: false }}  
+    />
+        
+    <MainStack.Screen 
+      name="Ato14" 
+      component={Ato14}
+      options={{ headerShown: false }}  
+    />
+            
+    <MainStack.Screen 
+      name="Ato15" 
+      component={Ato15}
+      options={{ headerShown: false }}  
+    />
+            
+    <MainStack.Screen 
+      name="Ato16" 
+      component={Ato16}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato17" 
+      component={Ato17}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato18" 
+      component={Ato18}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato19" 
+      component={Ato19}
+      options={{ headerShown: false }}  
+    />
+    
+    <MainStack.Screen 
+      name="Ato20" 
+      component={Ato20}
       options={{ headerShown: false }}  
     />
 
---
+++ App/screens/Ato3.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { 
+  SafeAreaView, 
+  ScrollView, 
+  TouchableOpacity, 
+  Linking, 
+  Alert,
+  StatusBar,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
+
+import { Entypo } from '@expo/vector-icons';
+
+import colors from '../constants/colors';
+
+import { TextItem } from '../components/TextItem'
+
+import { TextItemThree } from '../components/TextItemThree'
+
+const screen = Dimensions.get('window');
+
+const openUrl = (url) => {
+  return Linking.openURL(url).catch(() => {
+  Alert.alert('Desculpe, algo deu errado.', 'Por favor tente de novo mais tarde.');
+});
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    flex: 1,
+  },
+  image: {
+    paddingTop: 300,
+    width: screen.width * 0.45,
+    height: screen.height * 0.15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textStyleOne: {
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 15,
+  }
+});
+
+export default ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <StatusBar barStyle="dark-content" backgroundColor={colors.white} />
+
+        <TouchableOpacity style={{alignItems: 'center'}} onPress={() => navigation.push('Home')}>
+          <Entypo name="home" size={50} color={colors.black} style={{paddingTop: 5}} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.textStyleOne} onPress={() => navigation.push('Home')}>
+          <TextItem style={styles.textStyleOne} text="Ato 3" />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => openUrl('https://www.kurytibametropole.org/')}>
+          <TextItemThree 
+            text="Esse é o Ato número 3" 
+          />
+        </TouchableOpacity>
+
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
